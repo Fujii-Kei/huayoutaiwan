b3f65fddf209f84875bab7f168fd3f423a456cda
fujiikei <azyuyu@gmail.com>
2023-09-04 02:44:50 +0900

    admin_categories

--- app/uploaders/category_uploader.rb
+++ app/uploaders/category_uploader.rb
@@ -7,6 +7,14 @@ class CategoryUploader < CarrierWave::Uploader::Base
   storage :file
   # storage :fog
 
+  if Rails.env.production?
+    include Cloudinary::CarrierWave
+    CarrierWave.configure do |config|
+      config.cache_storage = :file
+    end
+  else
+    storage :file
+  end
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
   def store_dir
