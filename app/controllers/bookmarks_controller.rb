fb51f876ab1c62b4c4d653b245e087b52d363bff
fujiikei <azyuyu@gmail.com>
2023-09-21 18:57:24 +0900

    bookmark_button

---
+++ app/controllers/bookmarks_controller.rb
@@ -0,0 +1,25 @@
+class BookmarksController < ApplicationController
+  def index
+    @bookmarks = current_user.bookmarks.includes(:question)
+  end
+
+  def create
+    @question = Question.find(params[:question_id])
+    current_user.bookmark(@question)
+    render turbo_stream: turbo_stream.replace(
+      "bookmark-button-for-question-#{@question.id}-not-bookmarked",
+      partial: 'results/bookmark_button',
+      locals: { question: @question }
+    )
+  end
+
+  def destroy
+    @question = Question.find(params[:question_id])
+    current_user.unbookmark(@question)
+    render turbo_stream: turbo_stream.replace(
+      "bookmark-button-for-question-#{@question.id}",
+      partial: 'results/bookmark_button',
+      locals: { question: @question }
+    )
+  end
+end
